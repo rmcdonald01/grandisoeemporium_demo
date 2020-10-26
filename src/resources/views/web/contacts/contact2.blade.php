d840d450685510b02c4d5e0384e551bec95e3843
root <root@DESKTOP-M6D2JV3.localdomain>
2020-10-26 12:40:22 -0500

    contact info update

--- src/resources/views/web/contacts/contact2.blade.php
+++ src/resources/views/web/contacts/contact2.blade.php
@@ -63,7 +63,7 @@
               <div class="col-6">
                   
                   <ul class="contact-logo pl-0 mb-0">
-                    <li> <i class="fas fa-mobile-alt"></i><br>@lang('website.CONTACT US') </li>
+                    <li> <i class="fas fa-mobile-alt"></i><br>@lang('website.CONTACT US')</li>
                     <li> <i class="fas fa-map-marker"></i><br>@lang('website.ADDRESS')
                     </li>
                     <li> <i class="fas fa-envelope"></i><br>@lang('website.EMAIL ADDRESS') </li>
@@ -72,10 +72,8 @@
               </div>  
               <div class="col-6 right">
                 <ul class="contact-info  pl-0 mb-0">
-                  <li><font>
-                    <a href="#" dir="ltr"><br>{{$result['commonContent']['setting'][11]->value}}</a>
-                  </font> </li>
-                  <li> <font><a href="#">@lang('website.Ecommerce')<br>@lang('website.Demo Store 3654123')</a></font></li>
+                  <li><font><a href="#" dir="ltr"><br>{{$result['commonContent']['setting'][11]->value}}</a></font></li>
+                  <li> <font><a href="#">9834 Bobolink Dr Conroe Tx, 77385 United States</a></font></li>
                   <li> <font><a href="mailto:{{$result['commonContent']['setting'][3]->value}}"><br>{{$result['commonContent']['setting'][3]->value}}</a> </font></li>
                   <li><font><a href="#" dir="ltr"><br>{{$result['commonContent']['setting'][11]->value}}</a> </font></li>
                 </ul>
@@ -84,7 +82,7 @@
               
              
              <p style="margin-top:30px;"">
-              @lang('website.Contact us text')
+              <!-- @lang('website.Contact us text') -->
              </p>
           </div>
        
---
+++ src/storage/framework/views/9f3e3954660782ba3f42c3fe202a23b8b032e036.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<!-- meta contains meta taga, css and fontawesome icons etc -->
+<?php echo $__env->make('admin.common.meta', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<!-- ./end of meta -->
+
+<body class=" hold-transition skin-blue sidebar-mini">
+<!-- wrapper -->
+<div class="wrapper">
+
+    <div class="se-pre-con" id="loader" style="/* display: none; */">
+        <div class="pre-loader">
+          <div class="la-line-scale">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <p><?php echo app('translator')->get('labels.Loading'); ?>..</p>
+        </div>
+     
+      </div>
+
+    <!-- header contains top navbar -->
+<?php echo $__env->make('admin.common.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<!-- ./end of header -->
+
+    <!-- left sidebar -->
+<?php echo $__env->make('admin.common.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<!-- ./end of left sidebar -->
+
+    <!-- dynamic content -->
+<?php echo $__env->yieldContent('content'); ?>
+<!-- ./end of dynamic content -->
+
+    <!-- right sidebar -->
+<?php echo $__env->make('admin.common.controlsidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<!-- ./right sidebar -->
+    <?php echo $__env->make('admin.common.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</div>
+<!-- ./wrapper -->
+
+<!-- all js scripts including custom js -->
+<?php echo $__env->make('admin.common.scripts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<!-- ./end of js scripts -->
+
+</body>
+</html>
+<?php /**PATH /var/www/grandisoeemporium_demo/src/resources/views/admin/layout.blade.php ENDPATH**/ ?>
